5dfbdaed782dd097c048dd09a5c60dcaebb5d7ac
José Lucas <lsbrevem444@gmail.com>
2026-08-16 11:45:06 -0300

    feat: AuthService com metodo login (bcrypt.compare e geracao de JWT)

--- Backend/src/services/auth.service.ts
+++ Backend/src/services/auth.service.ts
@@ -1,5 +1,7 @@
 import bcrypt from 'bcrypt';
 import { prisma } from '../config/prisma.js';
+import jwt from 'jsonwebtoken';
+
 
 export class AuthService {
   async register(name: string, email: string, password: string) {
@@ -20,5 +22,36 @@ export class AuthService {
     });
 
     return user;
+  }
+
+
+  async login(email: string, password: string) {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Email ou senha inválidos');
+    }
+
+
+    const passwordMatches = await bcrypt.compare(password, user.passwordHash);
+
+    if (!passwordMatches) {
+      throw new Error('Email ou senha inválidos');
+    }
+
+    
+    const jwtSecret = process.env.JWT_SECRET;
+
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET não configurado');
+    }
+
+    const token = jwt.sign({ sub: user.id }, jwtSecret, {
+      expiresIn: '1d',
+    });
+
+    return { token };
   }
 }
